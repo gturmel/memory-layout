de99ec6559b62a171ed86d31ea7cfcce051b972f
Greg Turmel <gregturmel@gmail.com>
2016-02-18 22:10:05 -0500

    add a clock

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,34 @@
 // Testing Animations!
 $(function(){
-   "use strict";
+   'use strict';
+
+// this is the start of what will be the flip
+
+$(".tile").on("click", function(){
+   $(this).css("background-color", "red");
+   $(this).css("color", "white");
+});
+
+// This is the timer! class memory-time
+
+var hours = new Date().getHours();
+if (hours > 12) {
+   hours = hours -12
+};
+var minutes = new Date().getMinutes();
+var seconds = new Date().getSeconds();
+
+
+$(".memory-time").text(hours + " " + minutes + " " +seconds);
+
+});
+
+
+
 
    // part 1: set the events (click and 1.2function that I want to run on the click)
    // 3 animate alllll the properties. webkit transform all the things. css attributes need to get switched up.
-$(this).on("click", function()
-   (".tile").animate({
-      --webkit-transform: rotateY(180)
-   }
-   )
-)
+
 
 
 // this is the closing tag
-});
