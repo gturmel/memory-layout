b17ea53da88dd4024ff18dc0bf8bacb404e1a4f2
Greg Turmel <gregturmel@gmail.com>
2016-02-21 20:43:04 -0500

    add delay.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,17 +2,41 @@ $(function(){
    'use strict';
 
 // The function to flip the tiles. Will also add the class of the clicked tile to the [classArray]
-
+var clickCounter = 0;
 var classArray = [];
+var memoryLives = $(".memory-lives").children();
+var totalMatches = 0;
 
 function clickTile() {
+   clickCounter++;
    $(this).addClass("clicked");
+// pointer-events: none will stop the same one from getting clicked on
    classArray.push($(this).html());
    console.log(classArray);
 
    if (classArray.length === 2) {
       if(classArray[0] === classArray[1]){
-         console.log("this works");
+         console.log("You found a match");
+// this sets the cards to not rotate back when a match is found
+         $(classArray).each(function(){
+         $(".clicked").addClass("found").removeClass("clicked")});
+// resets the classArray to zero so I can get two new classes
+         classArray.pop();
+         classArray.pop();
+      }
+      else if (classArray[0] !== classArray[1]){
+         console.log("These don't match");
+         $(classArray).each(function(){
+         $(".clicked").removeClass("clicked").addClass("tile")});
+// this empties out the array, so I can get two new things to compare
+         classArray.pop();
+         classArray.pop();
+// this is the part that removes the hearts
+         $(memoryLives[clickCounter / 2]).detach();
+// alerts if you get to zero lives
+         if(memoryLives.length === 0){
+            alert("You ran out of tries.")
+         }
       }
    }
 
